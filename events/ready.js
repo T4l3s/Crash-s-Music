d77d6a2bccffabacb466bf5810711a8a3190e949
T4l3s <72084647+T4l3s@users.noreply.github.com>
2022-05-26 15:40:32 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 module.exports = async (client) => {
   client.Ready = true, 
   client.user.setPresence({
-    status: "online",  // You can show online, idle, and dnd
+    status: "dnd",  // You can show online, idle, and dnd
     activity: {
-        name: "Music",  // The message shown
-        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        name: "Hentai",  // The message shown
+        type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING,
     }
 });
     client.Manager.init(client.user.id);
